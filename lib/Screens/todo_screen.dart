d03c78e48fbd9b62cab7ffad6b5d3a7b7c3ee4e9
ftmkyz <fatmaakyuz96@gmail.com>
2024-05-28 21:38:04 +0300

    backup

--- lib/Screens/todo_screen.dart
+++ lib/Screens/todo_screen.dart
@@ -31,6 +31,11 @@ class _MyHomePageState extends State<MyHomePage> {
   late SharedPreferences _prefs;
   final TextEditingController _controller = TextEditingController();
   List<String> todos = [];
+  Color backgroundColor = const Color.fromARGB(255, 228,213,183);
+  Color themeColor = const Color.fromARGB(255, 135,174,115);
+  Color textColor = const Color.fromARGB(155, 0,33,71);
+  Color buttonbackground = const Color.fromARGB(255, 177,86,36);
+  final bool _visible = true;
 
   @override
   void initState() {
@@ -49,23 +54,44 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: themeColor,
       appBar: AppBar(
-        title: const Text('To-Do App'),
-        // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
+          toolbarHeight: 90,
+          title: Column(
+            children: [
+              // SizedBox(height: 8.0),
+              Text(
+                'To-Do App',
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  color: textColor,
+                ),
+              ),
+            ],
+          ),
+          // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          backgroundColor: themeColor),
       body: SingleChildScrollView(
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 18.0),
-              TextField(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const SizedBox(height: 18.0),
+            Container(
+              margin: const EdgeInsets.only(left: 6, right: 6),
+              child: TextField(
                 keyboardType: TextInputType.multiline,
                 maxLines: null,
                 controller: _controller,
                 decoration: InputDecoration(
+                  fillColor: backgroundColor,
+                  labelStyle: TextStyle(color: textColor),
+                  filled: true,
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50.0),
+                    borderSide: BorderSide(
+                        color: textColor, width: 0.0, style: BorderStyle.solid),
+                    borderRadius: BorderRadius.circular(15.0),
                   ),
                   labelText: 'Add new task',
                   suffixIcon: IconButton(
@@ -73,52 +99,120 @@ class _MyHomePageState extends State<MyHomePage> {
                       _saveTodo(_controller.text);
                     },
                     icon: const Icon(Icons.add),
-                    color: Colors.green,
+                    color: textColor,
                   ),
                 ),
               ),
-              const SizedBox(height: 28.0),
-              for (var i = 0; i < todos.length; i++)
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 1.0,
-                      ),
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          todos[i],
-                          style: const TextStyle(fontSize: 24.0),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: <Widget>[
-                            IconButton(
-                              onPressed: () {
-                                // Button action
-                              },
-                              icon: const Icon(Icons.delete, size: 22),
-                            ),
-                            IconButton(
-                              onPressed: () {
-                                // Button action
-                              },
-                              icon: const Icon(Icons.check, size: 22),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+            ),
+            const SizedBox(height: 28.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(left: 3),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: buttonbackground,
+                        foregroundColor: textColor),
+                    child: const Text("All Task"),
                   ),
                 ),
-            ]),
+                Container(
+                  margin: const EdgeInsets.only(left: 6),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: buttonbackground,
+                        foregroundColor: textColor),
+                    child: const Text("Pending Task"),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 6),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: buttonbackground,
+                        foregroundColor: textColor),
+                    child: const Text("Completed"),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 28.0),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: todos.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: AnimatedOpacity(
+                    opacity: _visible ? 1.0 : 0.0,
+                    duration: const Duration(milliseconds: 500),
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 3, right: 3),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: buttonbackground,
+                          width: 0.0,
+                        ),
+                        // borderRadius: BorderRadius.circular(30.0),
+                        color: backgroundColor,
+                        boxShadow: [
+                          BoxShadow(
+                            color: buttonbackground.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(
+                                0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            todos[index],
+                            style: TextStyle(fontSize: 24.0, color: textColor),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              IconButton(
+                                onPressed: () {
+                                  _deleteTodo(index);
+                                },
+                                icon: Icon(
+                                  Icons.delete,
+                                  size: 22,
+                                  color: buttonbackground,
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  // Button action
+                                },
+                                icon: Icon(
+                                  Icons.check,
+                                  size: 22,
+                                  color: buttonbackground,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -126,6 +220,15 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> _saveTodo(String todo) async {
     setState(() {
       todos.add(todo);
+      _controller.clear();
+      // print(todos);
+    });
+    await _prefs.setStringList('todos', todos);
+  }
+
+  Future<void> _deleteTodo(int id) async {
+    setState(() {
+      todos.removeAt(id);
       // print(todos);
     });
     await _prefs.setStringList('todos', todos);
